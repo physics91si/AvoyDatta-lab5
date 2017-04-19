f27501e36dc2bae1705bba6ab947915a4cf6ede9
Avoy Datta <avoy.dattal@stanford.edu>
2017-04-18 18:51:56 -0700

    Lab5 DONE!

--- data_analysis.py
+++ data_analysis.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
+X_SPACING = 0.001
+
+threshold_freq = 5
+
 def wavepacket(x, k, sigma):
     """This function creates a wavepacket on the interval defined by x with
     wavevector k and standard deviation sigma."""
@@ -17,6 +21,17 @@ def main():
     #TODO add your code here
 
 
+    x = np.arange(0, 5, X_SPACING)
+    y = noisy_packet(x, 5, 1, 0.2)
+
+    plt.plot(x, y)
+    #plt.show()
+
+    z = clean_data(x, y)
+
+    plt.plot(x, z)
+    plt.show()
+
 def noisy_packet(x_values, k, sigma, noise_amplitude):
     """This function returns a noisy Gaussian wavepacket with wave
     vector k, standard deviation sigma and Gaussian noise of standard
@@ -26,11 +41,17 @@ def noisy_packet(x_values, k, sigma, noise_amplitude):
     return noisy_y
 
 def clean_data(x_values,y_values):
-    """This function should take a set of y_values, perform the Fourier
-    transform on it, filter out the high frequency noise, transform the
-    signal back into real space, and return it."""
 
-    pass #TODO add your code here
-    #return y_clean
+	sorted_y = np.fft.rfft(y_values)
+	clean_freq = np.fft.rfftfreq(len(x_values), X_SPACING)
+
+	for i in range(0, len(clean_freq)):
+		if clean_freq[i] > threshold_freq:
+			sorted_y[i] = 0
+	clean_y = np.fft.irfft(sorted_y)
+
+	return clean_y
+
+
 
 main()  # calls your main function
--- trig.py
+++ trig.py
@@ -4,10 +4,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.arange(0, np.pi, .001);
+print (x)
+y = np.sin(x)
+print (y)
+plt.plot(x, y) 
+#plt.show()
 
 # TODO fill in this function
 def integrate(y, dx):
-    pass
+	counter = 0
+	for i in range(0, len(y)):
+		counter += dx * y[i] 
+	return counter
 
+p = integrate(np.sin(x), 0.05)
+print (p)
+
+
+		
 # TODO write code here to setup arrays x and y = sin(x) and then plot them.
 # After this is done implement your integrate function above integrate y
